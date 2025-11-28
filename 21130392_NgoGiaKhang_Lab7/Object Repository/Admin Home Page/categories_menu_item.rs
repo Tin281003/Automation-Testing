e8c0db5b509e25d7f1117eb6d574d3c9e5e2e488
NJaKhang <115335597+NJaKhang@users.noreply.github.com>
2025-11-28 23:34:06 +0700

    Add files via upload

---
+++ 21130392_NgoGiaKhang_Lab7/Object Repository/Admin Home Page/categories_menu_item.rs
@@ -0,0 +1,176 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>categories_menu_item</name>
+   <tag></tag>
+   <elementGuidId>beb8cad9-e9bc-42a3-815b-1320c03343aa</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>#cms-plugins-product-categories</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//a[@id='cms-plugins-product-categories']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+      <webElementGuid>2dfe113e-ba57-42fd-84d8-9b5bae1bb0b9</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>dropdown-item nav-priority-90</value>
+      <webElementGuid>5cc3e5ec-e1a2-4414-aba8-0fa869a73ced</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>href</name>
+      <type>Main</type>
+      <value>https://nest.botble.com/admin/ecommerce/product-categories</value>
+      <webElementGuid>d5cb9e5d-86b3-4ce5-8fde-bcd3876de204</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>cms-plugins-product-categories</value>
+      <webElementGuid>8b0063f1-841e-43a6-bc0b-2a46f0ed58b4</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>title</name>
+      <type>Main</type>
+      <value>Product categories</value>
+      <webElementGuid>ba0fb192-2c93-4e53-a434-78b93cfad281</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+            
+            
+  
+  
+  
+        
+    
+    
+        Product categories
+        
+    
+</value>
+      <webElementGuid>2f64d26d-3f0e-439d-a469-4584f2808ba5</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;cms-plugins-product-categories&quot;)</value>
+      <webElementGuid>4ee87a5b-097c-4e9d-9601-931d48cb460d</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:attributes</name>
+      <type>Main</type>
+      <value>//a[@id='cms-plugins-product-categories']</value>
+      <webElementGuid>067c1ac1-86a5-4157-a9e4-974c1b942c62</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='sidebar-menu']/ul/li[2]/div/a[10]</value>
+      <webElementGuid>5b9b9ba8-ec43-43a6-b422-31986047403d</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Product Inventory'])[1]/following::a[1]</value>
+      <webElementGuid>0903d78e-e5bb-4c9d-84b7-6f218ed039b4</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Product Prices'])[1]/following::a[2]</value>
+      <webElementGuid>3cf08ba3-77b4-4b4c-934d-159255bc3271</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Product tags'])[1]/preceding::a[1]</value>
+      <webElementGuid>f9c29b39-b0a7-44b9-a1c1-ec5e9cefed95</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:href</name>
+      <type>Main</type>
+      <value>//a[contains(@href, 'https://nest.botble.com/admin/ecommerce/product-categories')]</value>
+      <webElementGuid>684b4a54-e7bb-43ca-a761-262bedb904f6</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//li[2]/div/a[10]</value>
+      <webElementGuid>f0fbc2bd-7087-4252-a10a-b3b43ca30909</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//a[@href = 'https://nest.botble.com/admin/ecommerce/product-categories' and @id = 'cms-plugins-product-categories' and @title = 'Product categories' and (text() = '
+            
+            
+  
+  
+  
+        
+    
+    
+        Product categories
+        
+    
+' or . = '
+            
+            
+  
+  
+  
+        
+    
+    
+        Product categories
+        
+    
+')]</value>
+      <webElementGuid>81c6c309-9a90-428e-9f16-bf5d237297b8</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ 21130392_NgoGiaKhang_Lab7/Object Repository/Admin Login Page/login_submit_btn.rs
@@ -0,0 +1,178 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>login_submit_btn</name>
+   <tag></tag>
+   <elementGuidId>ee2380d4-3e9d-48a7-92a6-c76e5b561a96</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>div.form-footer</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//form[@id='botble-a-c-l-forms-auth-login-form']/div/div[3]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>017684e0-9071-4531-a5af-e1a5b327e383</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>form-footer</value>
+      <webElementGuid>82893ff0-8071-4a82-934f-2bcb3c2bddd2</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+
+    
+    
+
+    
+
+                                                                
+                
+                
+                        
+  
+  
+  
+            
+                        Sign in
+            
+        
+
+    
+    
+
+    
+
+                                                                
+                
+                </value>
+      <webElementGuid>37bdeabb-2597-4517-9c39-f3dfdf22758a</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;botble-a-c-l-forms-auth-login-form&quot;)/div[@class=&quot;form-body&quot;]/div[@class=&quot;form-footer&quot;]</value>
+      <webElementGuid>53edcba0-e7c2-47f9-ab17-096a5b9cac91</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//form[@id='botble-a-c-l-forms-auth-login-form']/div/div[3]</value>
+      <webElementGuid>7f18d40d-49c5-421d-982f-e0fc5377eeeb</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Remember me?'])[1]/following::div[1]</value>
+      <webElementGuid>08a933d2-b860-4964-8aac-16e620989bda</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Lost your password?'])[1]/following::div[3]</value>
+      <webElementGuid>4c0508ff-d30f-4659-83c8-23dc77945816</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Botble Technologies'])[1]/preceding::div[2]</value>
+      <webElementGuid>9478187f-8e36-4bb7-99fe-a67241852d30</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[3]</value>
+      <webElementGuid>2845008a-c06e-4e7e-951d-4d80cfeb7697</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//div[(text() = '
+
+    
+    
+
+    
+
+                                                                
+                
+                
+                        
+  
+  
+  
+            
+                        Sign in
+            
+        
+
+    
+    
+
+    
+
+                                                                
+                
+                ' or . = '
+
+    
+    
+
+    
+
+                                                                
+                
+                
+                        
+  
+  
+  
+            
+                        Sign in
+            
+        
+
+    
+    
+
+    
+
+                                                                
+                
+                ')]</value>
+      <webElementGuid>b0992f55-a62f-4b3b-b2e0-b4431650cff5</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
